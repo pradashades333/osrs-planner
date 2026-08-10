eaa96c9b5b72667635035200c47afc6e256d4d1f
pradashades333 <alexdreve03@gmail.com>
2026-08-10 13:53:01 +0200

    Generate unified plan

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import playerRouter from './routes/player.js'
 import planRouter from './routes/plan.js'
 import trainRouter from './routes/train.js'
+import questsRouter from './routes/quests.js'
 
 const app = express()
 
@@ -16,6 +17,7 @@ app.get('/', (_req, res) => {
 app.use('/api/train', trainRouter)
 app.use('/api/player', playerRouter)
 app.use('/api/plan', planRouter)
+app.use('/api/quests', questsRouter)
 
 app.use((_req, res) => {
   res.status(404).json({ error: 'Route not found' })
