27b0c39f131aacb3b88309696f12e32e634786d7
pradashades333 <alexdreve03@gmail.com>
2026-08-11 18:12:09 +0200

    Add API health response

--- server/app.js
+++ server/app.js
@@ -8,8 +8,22 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.get('/', (_req, res) => {
+  res.json({ status: 'ok', service: 'osrs-planner-api' })
+})
+
 app.use('/api/train', trainRouter)
 app.use('/api/player', playerRouter)
 app.use('/api/plan', planRouter)
+
+app.use((_req, res) => {
+  res.status(404).json({ error: 'Route not found' })
+})
+
+app.use((err, _req, res, _next) => {
+  console.error(err)
+  res.status(500).json({ error: 'Internal server error' })
+})
 
 export default app
